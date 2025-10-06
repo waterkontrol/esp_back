b74e07903dfd56afd5b40869055170aa6736a93e
inyelber <inyelber@gmail.com>
2025-10-06 16:06:24 -0400

    Preparado para Railway

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Middleware para recibir JSON
-app.use(express.json());
-
-// Ruta principal
 app.get('/', (req, res) => {
   res.send('Servidor ESP32 prueba funcionando!');
-});
-
-// Ruta de prueba para recibir datos del ESP32
-app.post('/esp-data', (req, res) => {
-  console.log('Datos recibidos del ESP32:', req.body);
-  res.json({ status: 'OK' });
 });
 
 app.listen(port, () => {
