8eb52f6e4e661b3f5e41cfc9f7a054337650c7f0
inyelber <inyelber@gmail.com>
2025-10-06 18:59:39 -0400

    Actualizar index.js para recibir POST desde ESP32

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3977;
 
 // Middleware
 app.use(express.json());
